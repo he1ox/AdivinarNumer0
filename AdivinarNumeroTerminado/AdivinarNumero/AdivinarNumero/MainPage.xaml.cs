46a7882cb2fd0e58d9cb4cdb323b4cb899716bb0
jorge lopez <georgev9000@gmail.com>
2021-03-05 20:22:06 -0600

    version final

---
+++ AdivinarNumeroTerminado/AdivinarNumero/AdivinarNumero/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AdivinarNumero
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculo_Clicked(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(resultado.Text)) 
+                //Si el usuario deja vacio el entry resultado.
+            {
+                DisplayAlert("ERROR!", "No puedes dejar el campo vacio", "Entiendo");
+            }
+            else
+            {
+                /* Suponiendo que el usuario haya hecho las operaciones correctas
+                    1. Se elimina el ultimo digito, dividendo entre 10*/
+                try
+                {
+                    var numero = (Convert.ToInt32(resultado.Text) / 10) - 4;
+                    DisplayAlert("El numero que pensaste es...", numero.ToString(), "Ok"); 
+                }
+                catch (FormatException)
+                {
+                    DisplayAlert("ERROR!", "Dato no valido", "Ok");
+                }
+            }
+        }
+    }
+}
